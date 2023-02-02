47d2739e702bfe090ec66963388f00eb154cc117
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-02-02 17:01:10 +1300

    Enhancement: Add compile options to gps_telemetry.ino
    Signed-off-by: Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>

--- examples/gps_telemetry/gps_telemetry.ino
+++ examples/gps_telemetry/gps_telemetry.ino
@@ -12,6 +12,15 @@
 #include "CRSFforArduino.h"
 #include "gps.h"
 
+#define PRINT_RAW_RC_CHANNELS 0
+#define PRINT_GPS_DATA 1
+
+#if (CRSF_USE_TELEMETRY == 0)
+#error "CRSF_USE_TELEMETRY must be enabled in CRSFconfig.h to use this example."
+#elif (CRSF_TELEMETRY_DEVICE_GPS == 0)
+#error "CRSF_TELEMETRY_DEVICE_GPS must be enabled in CRSFconfig.h to use this example."
+#endif
+
 // Create a GPS object.
 const uint32_t GPSRxPin = 4;
 const uint32_t GPSTxPin = 5;
@@ -23,12 +32,14 @@ CRSFforArduino crsf = CRSFforArduino(&Serial1);
 
 void setup()
 {
+#if (PRINT_RAW_RC_CHANNELS > 0) || (PRINT_GPS_DATA > 0)
     // Initialize the serial port & wait for the port to open.
     Serial.begin(115200);
     while (!Serial)
     {
         ;
     }
+#endif
 
     // Initialize the CRSFforArduino library.
     crsf.begin();
@@ -36,17 +47,20 @@ void setup()
     // Initialize the GPS sensor.
     gps.begin();
 
+#if (PRINT_RAW_RC_CHANNELS > 0) || (PRINT_GPS_DATA > 0)
     // Show the user that the sketch is ready.
     Serial.println("GPS Telemetry Example");
     delay(1000);
     Serial.println("Ready");
     delay(1000);
+#endif
 }
 
 void loop()
 {
     if (crsf.update())
     {
+#if (PRINT_RAW_RC_CHANNELS > 0)
         Serial.print("RC Channels <A: ");
         Serial.print(crsf.rcToUs(crsf.getChannel(1)));
         Serial.print(", E: ");
@@ -64,6 +78,7 @@ void loop()
         Serial.print(", Aux4: ");
         Serial.print(crsf.rcToUs(crsf.getChannel(8)));
         Serial.println(">");
+#endif
     }
 
     // Update the GPS sensor.
@@ -71,6 +86,22 @@ void loop()
     {
         // Send the GPS telemetry data to the CRSF receiver.
         crsf.writeGPStelemetry(gps.data.latitude, gps.data.longitude, gps.data.altitude, gps.data.speed, gps.data.heading, gps.data.satellites);
+
+#if (PRINT_GPS_DATA > 0)
+        Serial.print("GPS <Lat: ");
+        Serial.print(gps.data.latitude, 6);
+        Serial.print(", Lon: ");
+        Serial.print(gps.data.longitude, 6);
+        Serial.print(", Alt: ");
+        Serial.print(gps.data.altitude);
+        Serial.print(", Speed: ");
+        Serial.print(gps.data.speed);
+        Serial.print(", Heading: ");
+        Serial.print(gps.data.heading);
+        Serial.print(", Satellites: ");
+        Serial.print(gps.data.satellites);
+        Serial.println(">");
+#endif
     }
 }
 
