2deba6f7426212a7d621cd734a621e60fed0adbc
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-02-08 21:20:56 +1300

    Enahncement: Add BusIO I2C abstraction layer.
    Signed-off-by: Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>

---
+++ examples/ahrs_telemetry/busio_i2c_target.cpp
@@ -0,0 +1,286 @@
+/**
+ * @file busio_i2c_target.cpp
+ * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
+ * @brief A BusIO library for I2C Target Mode, based on Adafruit's BusIO library.
+ * @version 0.2.0
+ * @date 2023-02-08
+ *
+ * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
+ *
+ */
+
+#include "busio_i2c_target.h"
+
+ //#define DEBUG_SERIAL Serial
+
+ /*!
+  *    @brief  Create an I2C device at a given address
+  *    @param  addr The 7-bit I2C address for the device
+  *    @param  theWire The I2C bus to use, defaults to &Wire
+  */
+BusIO_I2C_Target::BusIO_I2C_Target(uint8_t addr, I2C* theWire) {
+  _addr = addr;
+  _wire = theWire;
+  _begun = false;
+  _maxBufferSize = 250; // as defined in Wire.h's RingBuffer
+}
+
+/*!
+ *    @brief  Initializes and does basic address detection
+ *    @param  addr_detect Whether we should attempt to detect the I2C address
+ * with a scan. 99% of sensors/devices don't mind but once in a while, they spaz
+ * on a scan!
+ *    @return True if I2C initialized and a device with the addr found
+ */
+bool BusIO_I2C_Target::begin(bool addr_detect) {
+  Serial.println("BusIO_I2C_Target::begin()");
+  _wire->begin();
+  _begun = true;
+
+  if (addr_detect) {
+    bool _detected = detected();
+    Serial.println("BusIO_I2C_Target::begin() - detected() - returned " + String(_detected));
+    return _detected;
+  }
+
+  Serial.println("BusIO_I2C_Target::begin() - done");
+  return true;
+}
+
+/*!
+ *    @brief  De-initialize device, turn off the Wire interface
+ */
+void BusIO_I2C_Target::end(void) {
+  // Not all port implement Wire::end(), such as
+  // - ESP8266
+  // - AVR core without WIRE_HAS_END
+  // - ESP32: end() is implemented since 2.0.1 which is latest at the moment.
+  // Temporarily disable for now to give time for user to update.
+#if !(defined(ESP8266) ||                                                      \
+      (defined(ARDUINO_ARCH_AVR) && !defined(WIRE_HAS_END)) ||                 \
+      defined(ARDUINO_ARCH_ESP32))
+  _wire->end();
+  _begun = false;
+#endif
+}
+
+/*!
+ *    @brief  Scans I2C for the address - note will give a false-positive
+ *    if there's no pullups on I2C
+ *    @return True if I2C initialized and a device with the addr found
+ */
+bool BusIO_I2C_Target::detected(void) {
+  Serial.println("BusIO_I2C_Target::detected()");
+
+  // Init I2C if not done yet
+  if (!_begun && !begin()) {
+    Serial.println("BusIO_I2C_Target::detected() - begin() failed");
+    return false;
+  }
+
+  // A basic scanner, see if it ACK's
+  Serial.println("BusIO_I2C_Target::detected() - scanning");
+  _wire->beginTransmission(_addr);
+  if (_wire->endTransmission() == 0) {
+    Serial.println("BusIO_I2C_Target::detected() - detected");
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.println(F("Detected"));
+#endif
+    Serial.println("BusIO_I2C_Target::detected() - done");
+    return true;
+  }
+  Serial.println("BusIO_I2C_Target::detected() - not detected");
+#ifdef DEBUG_SERIAL
+  DEBUG_SERIAL.println(F("Not detected"));
+#endif
+  Serial.println("BusIO_I2C_Target::detected() - done");
+  return false;
+}
+
+/*!
+ *    @brief  Write a buffer or two to the I2C device. Cannot be more than
+ * maxBufferSize() bytes.
+ *    @param  buffer Pointer to buffer of data to write. This is const to
+ *            ensure the content of this buffer doesn't change.
+ *    @param  len Number of bytes from buffer to write
+ *    @param  prefix_buffer Pointer to optional array of data to write before
+ * buffer. Cannot be more than maxBufferSize() bytes. This is const to
+ *            ensure the content of this buffer doesn't change.
+ *    @param  prefix_len Number of bytes from prefix buffer to write
+ *    @param  stop Whether to send an I2C STOP signal on write
+ *    @return True if write was successful, otherwise false.
+ */
+bool BusIO_I2C_Target::write(const uint8_t* buffer, size_t len, bool stop,
+  const uint8_t* prefix_buffer,
+  size_t prefix_len) {
+  if ((len + prefix_len) > maxBufferSize()) {
+    // currently not guaranteed to work if more than 32 bytes!
+    // we will need to find out if some platforms have larger
+    // I2C buffer sizes :/
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.println(F("\tI2CDevice could not write such a large buffer"));
+#endif
+    return false;
+  }
+
+  _wire->beginTransmission(_addr);
+
+  // Write the prefix data (usually an address)
+  if ((prefix_len != 0) && (prefix_buffer != nullptr)) {
+    if (_wire->write(prefix_buffer, prefix_len) != prefix_len) {
+#ifdef DEBUG_SERIAL
+      DEBUG_SERIAL.println(F("\tI2CDevice failed to write"));
+#endif
+      return false;
+    }
+  }
+
+  // Write the data itself
+  if (_wire->write(buffer, len) != len) {
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.println(F("\tI2CDevice failed to write"));
+#endif
+    return false;
+  }
+
+#ifdef DEBUG_SERIAL
+
+  DEBUG_SERIAL.print(F("\tI2CWRITE @ 0x"));
+  DEBUG_SERIAL.print(_addr, HEX);
+  DEBUG_SERIAL.print(F(" :: "));
+  if ((prefix_len != 0) && (prefix_buffer != nullptr)) {
+    for (uint16_t i = 0; i < prefix_len; i++) {
+      DEBUG_SERIAL.print(F("0x"));
+      DEBUG_SERIAL.print(prefix_buffer[i], HEX);
+      DEBUG_SERIAL.print(F(", "));
+    }
+  }
+  for (uint16_t i = 0; i < len; i++) {
+    DEBUG_SERIAL.print(F("0x"));
+    DEBUG_SERIAL.print(buffer[i], HEX);
+    DEBUG_SERIAL.print(F(", "));
+    if (i % 32 == 31) {
+      DEBUG_SERIAL.println();
+    }
+  }
+
+  if (stop) {
+    DEBUG_SERIAL.print("\tSTOP");
+  }
+#endif
+
+  if (_wire->endTransmission(stop) == 0) {
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.println();
+    // DEBUG_SERIAL.println("Sent!");
+#endif
+    return true;
+  }
+  else {
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.println("\tFailed to send!");
+#endif
+    return false;
+  }
+}
+
+/*!
+ *    @brief  Read from I2C into a buffer from the I2C device.
+ *    Cannot be more than maxBufferSize() bytes.
+ *    @param  buffer Pointer to buffer of data to read into
+ *    @param  len Number of bytes from buffer to read.
+ *    @param  stop Whether to send an I2C STOP signal on read
+ *    @return True if read was successful, otherwise false.
+ */
+bool BusIO_I2C_Target::read(uint8_t* buffer, size_t len, bool stop) {
+  size_t pos = 0;
+  while (pos < len) {
+    size_t read_len =
+      ((len - pos) > maxBufferSize()) ? maxBufferSize() : (len - pos);
+    bool read_stop = (pos < (len - read_len)) ? false : stop;
+    if (!_read(buffer + pos, read_len, read_stop))
+      return false;
+    pos += read_len;
+  }
+  return true;
+}
+
+bool BusIO_I2C_Target::_read(uint8_t* buffer, size_t len, bool stop) {
+#if defined(TinyWireM_h)
+  size_t recv = _wire->requestFrom((uint8_t)_addr, (uint8_t)len);
+#elif defined(ARDUINO_ARCH_MEGAAVR)
+  size_t recv = _wire->requestFrom(_addr, len, stop);
+#else
+  size_t recv = _wire->requestFrom((uint8_t)_addr, (uint8_t)len, (uint8_t)stop);
+#endif
+
+  if (recv != len) {
+    // Not enough data available to fulfill our obligation!
+#ifdef DEBUG_SERIAL
+    DEBUG_SERIAL.print(F("\tI2CDevice did not receive enough data: "));
+    DEBUG_SERIAL.println(recv);
+#endif
+    return false;
+  }
+
+  for (uint16_t i = 0; i < len; i++) {
+    buffer[i] = _wire->read();
+  }
+
+#ifdef DEBUG_SERIAL
+  DEBUG_SERIAL.print(F("\tI2CREAD  @ 0x"));
+  DEBUG_SERIAL.print(_addr, HEX);
+  DEBUG_SERIAL.print(F(" :: "));
+  for (uint16_t i = 0; i < len; i++) {
+    DEBUG_SERIAL.print(F("0x"));
+    DEBUG_SERIAL.print(buffer[i], HEX);
+    DEBUG_SERIAL.print(F(", "));
+    if (len % 32 == 31) {
+      DEBUG_SERIAL.println();
+    }
+  }
+  DEBUG_SERIAL.println();
+#endif
+
+  return true;
+}
+
+/*!
+ *    @brief  Write some data, then read some data from I2C into another buffer.
+ *    Cannot be more than maxBufferSize() bytes. The buffers can point to
+ *    same/overlapping locations.
+ *    @param  write_buffer Pointer to buffer of data to write from
+ *    @param  write_len Number of bytes from buffer to write.
+ *    @param  read_buffer Pointer to buffer of data to read into.
+ *    @param  read_len Number of bytes from buffer to read.
+ *    @param  stop Whether to send an I2C STOP signal between the write and read
+ *    @return True if write & read was successful, otherwise false.
+ */
+bool BusIO_I2C_Target::write_then_read(const uint8_t* write_buffer,
+  size_t write_len, uint8_t* read_buffer,
+  size_t read_len, bool stop) {
+  if (!write(write_buffer, write_len, stop)) {
+    return false;
+  }
+
+  return read(read_buffer, read_len);
+}
+
+/*!
+ *    @brief  Returns the 7-bit address of this device
+ *    @return The 7-bit address of this device
+ */
+uint8_t BusIO_I2C_Target::address(void) { return _addr; }
+
+/*!
+ *    @brief  Change the I2C clock speed to desired (relies on
+ *    underlying Wire support!
+ *    @param desiredclk The desired I2C SCL frequency
+ *    @return True if this platform supports changing I2C speed.
+ *    Not necessarily that the speed was achieved!
+ */
+bool BusIO_I2C_Target::setSpeed(uint32_t desiredclk) {
+  _wire->setClock(desiredclk);
+  return true;
+}
+
---
+++ examples/ahrs_telemetry/busio_i2c_target.h
@@ -0,0 +1,45 @@
+/**
+ * @file busio_i2c_target.h
+ * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
+ * @brief A BusIO library for I2C Target Mode, based on Adafruit's BusIO library.
+ * @version 0.2.0
+ * @date 2023-02-08
+ *
+ * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include "Arduino.h"
+#include "i2c.h"
+
+ ///< The class which defines how we will talk to this device over I2C
+class BusIO_I2C_Target {
+public:
+    BusIO_I2C_Target(uint8_t addr, I2C* theWire = &Wire);
+    uint8_t address(void);
+    bool begin(bool addr_detect = true);
+    void end(void);
+    bool detected(void);
+
+    bool read(uint8_t* buffer, size_t len, bool stop = true);
+    bool write(const uint8_t* buffer, size_t len, bool stop = true,
+        const uint8_t* prefix_buffer = nullptr, size_t prefix_len = 0);
+    bool write_then_read(const uint8_t* write_buffer, size_t write_len,
+        uint8_t* read_buffer, size_t read_len,
+        bool stop = false);
+    bool setSpeed(uint32_t desiredclk);
+
+    /*!   @brief  How many bytes we can read in a transaction
+     *    @return The size of the Wire receive/transmit buffer */
+    size_t maxBufferSize() { return _maxBufferSize; }
+
+private:
+    uint8_t _addr;
+    I2C* _wire;
+    bool _begun;
+    size_t _maxBufferSize;
+    bool _read(uint8_t* buffer, size_t len, bool stop);
+};
+
