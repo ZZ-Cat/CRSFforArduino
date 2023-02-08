c113cc3496bf9ff21badad5fae38a06329515b89
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-02-09 12:03:01 +1300

    Chore: Code cleanup.
    Signed-off-by: Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>

--- examples/ahrs_telemetry/i2c.cpp
+++ examples/ahrs_telemetry/i2c.cpp
@@ -51,19 +51,13 @@ I2C::I2C(SERCOM* s, uint8_t pinSDA, uint8_t pinSCL)
  * @param baudrate The baudrate to use for I2C. Defaults to 100000.
  */
 void I2C::begin(uint32_t baudrate) {
-    Serial.println("I2C: I2C::begin()");
-
     // Controller Mode
-    Serial.println("I2C: I2C::begin() - sercom->initMasterWIRE()");
     sercom->initMasterWIRE(baudrate);
     sercom->enableWIRE();
-    Serial.println("I2C: I2C::begin() - sercom->enableWIRE()");
 
     // Force PIO_SERCOM_ALT for now.
     pinPeripheral(_uc_pinSDA, PIO_SERCOM_ALT);
     pinPeripheral(_uc_pinSCL, PIO_SERCOM_ALT);
-
-    Serial.println("I2C: I2C::begin() - done");
 }
 
 /**
@@ -161,15 +155,11 @@ uint8_t I2C::requestFrom(uint8_t address, size_t quantity)
 }
 
 void I2C::beginTransmission(uint8_t address) {
-    Serial.println("I2C: I2C::beginTransmission()");
-
     // save address of target and clear buffer
-    Serial.println("I2C: I2C::beginTransmission() - save address of target and clear buffer");
     txAddress = address;
     txBuffer.clear();
 
     transmissionBegun = true;
-    Serial.println("I2C: I2C::beginTransmission() - done");
 }
 
 // Errors:
@@ -180,43 +170,31 @@ void I2C::beginTransmission(uint8_t address) {
 //  4 : Other error
 uint8_t I2C::endTransmission(bool stopBit)
 {
-    Serial.println("I2C: I2C::endTransmission()");
     transmissionBegun = false;
 
     // Start I2C transmission
-    Serial.println("I2C: I2C::endTransmission() - Start I2C transmission");
     if (!sercom->startTransmissionWIRE(txAddress, WIRE_WRITE_FLAG))
     {
-        Serial.println("I2C: I2C::endTransmission() - Start I2C transmission - Send stop bit");
         sercom->prepareCommandBitsWire(WIRE_MASTER_ACT_STOP);
-        Serial.println("I2C: I2C::endTransmission() - Start I2C transmission - Address error");
         return 2;  // Address error
     }
-    Serial.println("I2C: I2C::endTransmission() - Start I2C transmission - done");
 
     // Send all buffer
-    Serial.println("I2C: I2C::endTransmission() - Send all buffer");
     while (txBuffer.available())
     {
         // Trying to send data
         if (!sercom->sendDataMasterWIRE(txBuffer.read_char()))
         {
-            Serial.println("I2C: I2C::endTransmission() - Send all buffer - Send stop bit");
             sercom->prepareCommandBitsWire(WIRE_MASTER_ACT_STOP);
-            Serial.println("I2C: I2C::endTransmission() - Send all buffer - Data error");
             return 3;  // Nack or error
         }
     }
-    Serial.println("I2C: I2C::endTransmission() - Send all buffer - done");
 
     if (stopBit)
     {
-        Serial.println("I2C: I2C::endTransmission() - Stop bit - Send stop bit");
         sercom->prepareCommandBitsWire(WIRE_MASTER_ACT_STOP);
-        Serial.println("I2C: I2C::endTransmission() - Stop bit - done");
     }
 
-    Serial.println("I2C: I2C::endTransmission() - done");
     return 0;
 }
 
