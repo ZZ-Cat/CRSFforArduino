4fc5ade29eafe8a7962bf77c09a87aa7503bd6aa
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-02-19 11:52:43 +1300

    revert(main): Revert main.cpp back to its default appearance.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,11 @@
 #include "Arduino.h"
-#include "CRSFforArduino.h"
-
-CRSFforArduino crsf = CRSFforArduino(&Serial1);
 
 void setup()
 {
-    // Initialize the serial port & wait for the port to open.
-    Serial.begin(115200);
-    while (!Serial)
-    {
-        ;
-    }
-
-    // Initialize the CRSFforArduino library.
-    crsf.begin();
-
-    // Show the user that the sketch is ready.
-    Serial.println("Channels Example");
-    delay(1000);
-    Serial.println("Ready");
-    delay(1000);
+    // put your setup code here, to run once:
 }
 
 void loop()
 {
-    if (crsf.update())
-    {
-        Serial.print("RC Channels <A: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(1)));
-        Serial.print(", E: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(2)));
-        Serial.print(", T: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(3)));
-        Serial.print(", R: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(4)));
-        Serial.print(", Aux1: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(5)));
-        Serial.print(", Aux2: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(6)));
-        Serial.print(", Aux3: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(7)));
-        Serial.print(", Aux4: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(8)));
-        Serial.println(">");
-    }
+    // put your main code here, to run repeatedly:
 }
