0724a7ad887a42c265c8b749acabc4852b56b897
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-02-25 18:50:22 +1300

    refactor(platformio): Move all PlatformIO files into src subfolder.
    Prep work for reinstating compatibility with the Arduino IDE.

---
+++ src/src/main_rc.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file main_rc.cpp
+ * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
+ * @brief This file demonstrates the full capabilities of CRSF for Arduino.
+ * @version 0.3.0
+ * @date 2023-02-25
+ *
+ * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
+ *
+ * @section License GNU General Public License v3.0
+ * This main file is a part of the CRSF for Arduino library.
+ * CRSF for Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CRSF for Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CRSF for Arduino.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+/* For some reason, main_rc.cpp is included in the Arduino IDE build as well as the PlatformIO build.
+ * This causes the Arduino IDE to complain about setup() and loop() being defined in multiple files.
+ * The following #if defined() block is used to prevent the Arduino IDE from complaining about setup() and loop()
+ * being defined in multiple files.
+ */
+#if defined(ARDUINO) && defined(PLATFORMIO)
+#include "Arduino.h"
+
+#include "CRSFforArduino.h"
+
+CRSFforArduino crsf = CRSFforArduino(&Serial1);
+
+void setup()
+{
+    // Initialize the serial port & wait for the port to open.
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        ;
+    }
+
+    // Initialize the CRSFforArduino library.
+    crsf.begin();
+
+    // Show the user that the sketch is ready.
+    Serial.println("RC Channels Example");
+    delay(1000);
+    Serial.println("Ready");
+    delay(1000);
+}
+
+void loop()
+{
+    if (crsf.update())
+    {
+        Serial.print("RC Channels <A: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(1)));
+        Serial.print(", E: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(2)));
+        Serial.print(", T: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(3)));
+        Serial.print(", R: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(4)));
+        Serial.print(", Aux1: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(5)));
+        Serial.print(", Aux2: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(6)));
+        Serial.print(", Aux3: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(7)));
+        Serial.print(", Aux4: ");
+        Serial.print(crsf.rcToUs(crsf.getChannel(8)));
+        Serial.println(">");
+    }
+}
+#endif // defined(PLATFORMIO)
