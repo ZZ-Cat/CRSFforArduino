f3802db85d7dd0dfbca2f41cef92262f10c7f9aa
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-07-20 11:39:57 +1200

    refactor(rc channels): Print RC channels to the Serial Monitor at 100 ms.
    This prevents the Serial Monitor from becoming flooded with data.

--- src/src/main_rc.cpp
+++ src/src/main_rc.cpp
@@ -66,23 +66,30 @@ void loop()
 {
     if (crsf.update())
     {
-        Serial.print("RC Channels <A: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(1)));
-        Serial.print(", E: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(2)));
-        Serial.print(", T: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(3)));
-        Serial.print(", R: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(4)));
-        Serial.print(", Aux1: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(5)));
-        Serial.print(", Aux2: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(6)));
-        Serial.print(", Aux3: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(7)));
-        Serial.print(", Aux4: ");
-        Serial.print(crsf.rcToUs(crsf.getChannel(8)));
-        Serial.println(">");
+
+        /* Print RC channels every 100 ms. Do this using the millis() function to avoid blocking the main loop. */
+        static unsigned long lastPrint = 0;
+        if (millis() - lastPrint >= 100)
+        {
+            lastPrint = millis();
+            Serial.print("RC Channels <A: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(1)));
+            Serial.print(", E: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(2)));
+            Serial.print(", T: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(3)));
+            Serial.print(", R: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(4)));
+            Serial.print(", Aux1: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(5)));
+            Serial.print(", Aux2: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(6)));
+            Serial.print(", Aux3: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(7)));
+            Serial.print(", Aux4: ");
+            Serial.print(crsf.rcToUs(crsf.getChannel(8)));
+            Serial.println(">");
+        }
     }
 }
 #endif // defined(PLATFORMIO)
