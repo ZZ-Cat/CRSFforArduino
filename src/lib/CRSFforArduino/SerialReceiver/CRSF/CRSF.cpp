a731e13f4740a2e2121936d577c6f451762d65c1
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-08-01 12:27:07 +1200

    refactor(library): Split CRSF for Arduino up into abstraction layers
    This is the initial commit of a total re-factor of CRSF for Arduino, with the end goal of the entire library taking advantage of abstraction layers.

---
+++ src/lib/CRSFforArduino/SerialReceiver/CRSF/CRSF.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file CRSF.cpp
+ * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
+ * @brief CRSF class implementation.
+ * @version 0.4.0
+ * @date 2023-08-01
+ *
+ * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
+ *
+ * @section License GNU General Public License v3.0
+ * This source file is a part of the CRSF for Arduino library.
+ * CRSF for Arduino is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CRSF for Arduino is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CRSF for Arduino.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
