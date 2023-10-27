31d023785a7c1cb6c043614bc3fbc606924f4a5f
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2023-10-28 11:15:18 +1300

    chore(`telemetry.ino`): Fix typo in boiler plate

--- examples/telemetry/telemetry.ino
+++ examples/telemetry/telemetry.ino
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
  *
  * @section License GNU General Public License v3.0
- * This [file type] is a part of the CRSF for Arduino library.
+ * This example sketch is a part of the CRSF for Arduino library.
  * CRSF for Arduino is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
