df8759be0862db5ee9918f28f5c15dcec997b680
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2024-01-15 15:16:22 +1300

    chore(library): Bump library version number and date to Version 1.0.0, 2024-1-15
    This marks the beginning of development of CRSF for Arduino Version 1.0.0

--- src/CRSFforArduino/src/SerialReceiver/SerialReceiver.hpp
+++ src/CRSFforArduino/src/SerialReceiver/SerialReceiver.hpp
@@ -2,8 +2,8 @@
  * @file SerialReceiver.hpp
  * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
  * @brief This is the header file for the Serial Receiver Interface.
- * @version 0.5.0
- * @date 2023-11-1
+ * @version 1.0.0
+ * @date 2024-1-15
  *
  * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
  *
