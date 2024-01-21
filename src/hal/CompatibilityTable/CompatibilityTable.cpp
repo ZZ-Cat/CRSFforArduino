2bc1628684a8c42a86608469e634f713da627241
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2024-01-22 09:29:53 +1300

    feat(devboard): Initial support for the Arduino RP2040 architecture.
    This is a precursor to adding support for RP2040-based development boards.

--- src/hal/CompatibilityTable/CompatibilityTable.cpp
+++ src/hal/CompatibilityTable/CompatibilityTable.cpp
@@ -95,6 +95,13 @@ namespace hal
         device.type.devboard = DEVBOARD_IS_PERMISSIVELY_INCOMPATIBLE_UNKNOWN_BOARD;
 #endif
 
+// Raspberry Pi RP2040 Architecture
+#elif defined(ARDUINO_ARCH_RP2040)
+
+        // The architecture and chip is known, but the board is not.
+#warning "The target board is unknown. Please enable CRSF_DEBUG_ENABLED and CRSF_DEBUG_ENABLE_COMPATIBILITY_TABLE_OUTPUT in CFA_Config.hpp for more information."
+        device.type.devboard = DEVBOARD_IS_PERMISSIVELY_INCOMPATIBLE_UNKNOWN_BOARD;
+
 // Arduino SAMD Architecture
 #elif defined(ARDUINO_ARCH_SAMD)
 
