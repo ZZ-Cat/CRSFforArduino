61f8f5715d95ac34826de90663fe46024d9a566f
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2024-01-21 22:08:24 +1300

    chore(library): Fix version date inconsistencies

--- examples/channels/channels.ino
+++ examples/channels/channels.ino
@@ -3,7 +3,7 @@
  * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
  * @brief This example sketch shows how to receive RC channels from a CRSF receiver using the CRSF for Arduino library.
  * @version 1.0.0
- * @date 2024-1-15
+ * @date 2024-1-20
  *
  * @copyright Copyright (c) 2023, Cassandra "ZZ Cat" Robinson. All rights reserved.
  *
