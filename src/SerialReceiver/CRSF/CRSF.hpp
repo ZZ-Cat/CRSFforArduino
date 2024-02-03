619cbc7c7ce230c91366530ee9ad3647d94685c2
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2024-02-04 09:45:59 +1300

    chore(serial receiver interface): Reinstate CRSF API

--- src/SerialReceiver/CRSF/CRSF.hpp
+++ src/SerialReceiver/CRSF/CRSF.hpp
@@ -15,6 +15,17 @@ namespace serialReceiverLayer
     public:
         CRSF();
         virtual ~CRSF();
+        void begin();
+        void end();
+        void setFrameTime(uint32_t baudRate, uint8_t packetCount = 10);
+        bool receiveFrames(uint8_t rxByte);
+        void getRcChannels(uint16_t *rcChannels);
     private:
+        bool rcFrameReceived;
+        uint16_t frameCount;
+        uint32_t timePerFrame;
+        crsfProtocol::frame_t rxFrame;
+        crsfProtocol::frame_t rcChannelsFrame;
+        uint8_t calculateFrameCRC();
     };
 } // namespace serialReceiverLayer
