80638567ba31b404f9f144324be846fa9e34fb3d
Cassandra "ZZ Cat" Robinson <nicad.heli.flier@gmail.com>
2024-02-18 09:13:40 +1300

    chore(library): Bump version date to `2024-2-18`

--- src/CFA_Config.hpp
+++ src/CFA_Config.hpp
@@ -3,7 +3,7 @@
  * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
  * @brief This is the configuration file for CRSF for Arduino.
  * @version 1.0.0
- * @date 2024-2-14
+ * @date 2024-2-18
  *
  * @copyright Copyright (c) 2024, Cassandra "ZZ Cat" Robinson. All rights reserved.
  *
