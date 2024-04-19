580b543123bd80d33f95f51c0d7dde4148caf338
ZZ-Cat <nicad.heli.flier@gmail.com>
2024-04-20 11:19:46 +1200

    chore(library): Bump version date to `2024-4-20`

--- src/SerialReceiver/CRC/CRC.cpp
+++ src/SerialReceiver/CRC/CRC.cpp
@@ -3,7 +3,7 @@
  * @author Cassandra "ZZ Cat" Robinson (nicad.heli.flier@gmail.com)
  * @brief A generic CRC8 implementation for the CRSF for Arduino library.
  * @version 1.1.0
- * @date 2024-4-18
+ * @date 2024-4-20
  *
  * @copyright Copyright (c) 2024, Cassandra "ZZ Cat" Robinson. All rights reserved.
  *
