e544c5ae2715e266f8cb15d554a4440117f56e6d
ZZ-Cat <nicad.heli.flier@gmail.com>
2024-04-26 19:40:57 +1200

    refactor(serial transmitter prototype): :recycle: Prepare CRSF RC Channels Packed data...
    ...and send it at the selected packet rate.

--- examples/platformio/serial_transmitter_prototype.cpp
+++ examples/platformio/serial_transmitter_prototype.cpp
@@ -262,6 +262,12 @@ void setup()
     rc_channels_packed.ch15 = 992;
     rc_channels_packed.ch16 = 992;
 
+    /* Prepare the CRSF RC Channels Packed frame. */
+    crsf_tx_frame.frame.sync = 0xC8; // NB: EdgeTX uses 0xEE which is incorrect.
+    crsf_tx_frame.frame.length = 24;
+    crsf_tx_frame.frame.type = 0x16;
+    memcpy(crsf_tx_frame.frame.payload, &rc_channels_packed, sizeof(rc_channels_packed));
+    crsf_tx_frame.frame.crc = calculate_crc(&crsf_tx_frame);
 
     /* Print a message to the serial monitor. */
     Serial.println("Testing CRSF Serial Transmitter Prototype...");
@@ -298,7 +304,7 @@ void loop()
             Serial1.write(crsf_tx_frame.buffer, crsf_tx_frame.frame.length + 1);
 
             /* Write 64 bytes to Serial1. */
-            Serial1.write(crsf_tx_frame.buffer, crsf_frame_size);
+            Serial1.write(crsf_tx_frame.buffer, crsf_tx_frame.frame.length + 1);
 
             /* Increment the iteration. */
             iteration++;
