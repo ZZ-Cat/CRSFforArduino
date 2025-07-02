4e78f604c47dabb3fbd9d5d292786bb371916011
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-07-02 21:05:22 +0000

    ci(quality control): :recycle: Pass `--deploy` as an argument to flash the `development` environment to its pre-configured development board
    If `--deploy` is not provided, the `development` environment is built but not flashed.

--- src/build/scripts/build.py
+++ src/build/scripts/build.py
@@ -2,6 +2,34 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
+    # Get the command line arguments.
+    args = sys.argv[1:]
+    # If --deploy was specified as a command line argument, deploy the code to the device.
+    if "--deploy" in args:
+        # Run the PlatformIO upload command.
+        command = "pio run -e development -t upload"
+        try:
+            subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(f"{e.stdout.strip()}")
+            print(f"{e.stderr.strip()}")
+            print("Deployment failed. Please resolve the issues before proceeding.")
+            exit(1)
+    # If no arguments were specified, run the build command.
+    else:
+        # Run the PlatformIO build command.
+        command = "pio run -e development"
+        try:
+            subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(f"{e.stdout.strip()}")
+            print(f"{e.stderr.strip()}")
+            print("Build failed. Please resolve the issues before proceeding.")
+            exit(1)
+
+    # Exit with code 0 if everything is fine.
+    exit(0)
+
     # # Run the PlatformIO build command
     # command = "pio run -e development"
     # try:
