d72eb2cf241a377dbe58f3d465a6a99201c9932c
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-08-29 06:34:57 +0000

    ci(quality control): :fire: Remove commented out code

--- src/build/scripts/build.py
+++ src/build/scripts/build.py
@@ -79,16 +79,3 @@ if __name__ == "__main__":
 
     # Exit with code 0 if everything is fine.
     exit(0)
-
-    # # Run the PlatformIO build command
-    # command = "pio run -e development"
-    # try:
-    #     subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
-    # except subprocess.CalledProcessError as e:
-    #     print(f"{e.stdout.strip()}")
-    #     print(f"{e.stderr.strip()}")
-    #     print("Tests failed. Please resolve the issues before proceeding.")
-    #     exit(1)
-
-    # # Exit with code 0 if everything is fine.
-    # exit(0)
