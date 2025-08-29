b58a00e17321fc12caf4df1a94bcd7248e877a4d
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-08-29 21:21:01 +0000

    chore(library): Bump Semantic Version to 1.1.0-2025.8.30
    This is the latest update for the Version-1.1.0-Development branch. Plus it makes the `is-my-QC-and-QL-still-broken` branch different enough to check whether-or-not my QC and Vulnerability Scanner is still broken.

--- src/CFA_Config.hpp
+++ src/CFA_Config.hpp
@@ -35,7 +35,7 @@ namespace crsfForArduinoConfig
 Versioning is done using Semantic Versioning 2.0.0.
 See https://semver.org/ for more information. */
 #define CRSFFORARDUINO_VERSION       "1.1.0"
-#define CRSFFORARDUINO_VERSION_DATE  "2024-7-21"
+#define CRSFFORARDUINO_VERSION_DATE  "2025-08-30"
 #define CRSFFORARDUINO_VERSION_MAJOR 1
 #define CRSFFORARDUINO_VERSION_MINOR 1
 #define CRSFFORARDUINO_VERSION_PATCH 0
@@ -46,11 +46,11 @@ See https://semver.org/ for more information. */
 // These are the pre-release version details which are only used if CRSFFORARDUINO_VERSION_IS_PRERELEASE is set to 1.
 // NOTE: Pre-release versions are not recommended for production use.
 #if CRSFFORARDUINO_VERSION_IS_PRERELEASE == 1
-#define CRSFFORARDUINO_VERSION_PRE   "1.0.0"
-#define CRSFFORARDUINO_VERSION_BUILD_DATE "2024-7-21"
-#define CRSFFORARDUINO_VERSION_BUILD_MAJOR 1
-#define CRSFFORARDUINO_VERSION_BUILD_MINOR 0
-#define CRSFFORARDUINO_VERSION_BUILD_PATCH 0
+#define CRSFFORARDUINO_VERSION_PRE   "2025.8.30"
+#define CRSFFORARDUINO_VERSION_BUILD_DATE "2025-08-30"
+#define CRSFFORARDUINO_VERSION_BUILD_MAJOR 2025
+#define CRSFFORARDUINO_VERSION_BUILD_MINOR 8
+#define CRSFFORARDUINO_VERSION_BUILD_PATCH 30
 #endif
 
 /* Failsafe Options
