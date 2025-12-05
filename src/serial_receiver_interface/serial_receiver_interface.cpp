9becb47fb62a60c4a28baf66df50f93793260bed
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-12-05 23:32:15 +0000

    chore: Add placeholders and boiler-plate to Serial Receiver Interface

---
+++ src/serial_receiver_interface/serial_receiver_interface.cpp
@@ -0,0 +1,5 @@
+#include <serial_receiver_interface.hpp>
+
+namespace __cfa_internal_middleware_serial_receiver_interface
+{
+} // namespace __cfa_internal_middleware_serial_receiver_interface
--- src/serial_receiver_interface/serial_receiver_interface.hpp
+++ src/serial_receiver_interface/serial_receiver_interface.hpp
@@ -7,5 +7,9 @@ namespace __cfa_internal_middleware_serial_receiver_interface
     public:
         serial_receiver_interface() = default;
         virtual ~serial_receiver_interface() = default;
+
+        // virtual size_t write(const uint8_t* data, size_t length) = 0;
+        // virtual size_t read(uint8_t* buffer, size_t length) = 0;
+        // virtual size_t available() = 0;
     };
 } // namespace __cfa_internal_middleware_serial_receiver_interface
