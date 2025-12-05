8c061d1caa671a6e211105e29c6f17834de926d3
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-12-05 07:25:44 +0000

    chore(firmware examples): :fire: Removed commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,6 @@
 #include <gsl/gsl>
 // NOLINTEND(misc-include-cleaner)
 
-// namespace my_namespace
-// {
-//     class class_a_base
-//     {
-//     public:
-//         // Default constructor.
-//         class_a_base() = default;
-
-//         // Copy constructor, copy assignment operator, move constructor, and move assignment operator.
-//         class_a_base(const class_a_base&) = default;
-//         auto operator=(const class_a_base &) -> class_a_base & = default;
-//         class_a_base(class_a_base &&) = default;
-//         auto operator=(class_a_base &&) -> class_a_base & = default;
-
-//         virtual ~class_a_base() = default;
-
-//         virtual void do_something() = 0;
-//     };
-
-//     class class_a_derived : private class_a_base
-//     {
-//     public:
-//         void do_something() override
-//         {
-//             Serial.println("Derived class doing something!");
-//         }
-
-//     };
-// } // namespace my_namespace
-
 extern void setup();
 extern void loop();
 
@@ -50,13 +20,7 @@ void setup()
     while (!Serial)
     {
         yield();
-        // delay(100);
     }
-
-    // Serial.println("Hello, World!");
-
-    // my_namespace::class_a_derived obj;
-    // obj.do_something();
 
     cfa_global = gsl::owner<CRSFforArduino *>(new CRSFforArduino());
     cfa_global->begin();
