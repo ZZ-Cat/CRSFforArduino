6fd1ec4c468ecd72ce72452bfff637ab1af346e9
ZZ-Cat <nicad.heli.flier@gmail.com>
2025-12-11 22:47:15 +0000

    chore(sketch API): :fire: Remove unneeded linter warning on `update()` function.

--- src/CRSFforArduino.cpp
+++ src/CRSFforArduino.cpp
@@ -2,14 +2,13 @@
 
 namespace CRSF_for_Arduino
 {
-    // NOLINTNEXTLINE(readability-convert-member-functions-to-static, readability-function-cognitive-complexity)
     void CRSFforArduino::update() // cppcheck-suppress unusedFunction
     {
         // Use the base class method to receive data frames.
-        if (receive_data_frame())
+        if (this->receive_data_frame())
         {
             // Valid frame received; parse it.
-            parse_data_frame();
+            this->parse_data_frame();
         }
     }
 } // namespace CRSF_for_Arduino
